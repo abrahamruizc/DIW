22a36ab6492c06e5be7b0b6be2eb71f4a287550d
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-12-06 17:05:46 +0100

    UD03-T01-EVALUABLE Terminado

---
+++ UD03-T01-Evaluable/js/script.js
@@ -0,0 +1,11 @@
+let video = document.getElementById("myVideo");
+let btn = document.getElementById("myBtn");
+let titulo = document.getElementById("h");
+
+function Cerrar() {
+    video.className = "cerrar";
+    btn.className = "cerrar";
+    titulo.className = "cerrar";
+}
+        
+        
