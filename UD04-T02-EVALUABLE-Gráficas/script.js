e23b6c3912cc6dc0f6d08d744bc8cb805f65b771
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-01-31 19:45:14 +0100

    graficos por la mitad

--- UD04-T02-EVALUABLE-Gráficas/script.js
+++ UD04-T02-EVALUABLE-Gráficas/script.js
@@ -2,36 +2,80 @@ window.onload = inicio;
 
 function inicio() {
 graficoLineal();
-
+graficoDosSeries();
 }
 
+function graficoDosSeries() {
+  const ctx = document.getElementById('series');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Enero', 'Febrero', 'marzo', 'abril', 'mayo', 'Junio', 'Julio', 'Agosto', 'septiembre', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre', 'diciembre'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      },{
+        type: 'line',
+        label: 'Line Dataset',
+        data: [50, 50, 50, 50],
+    }]
+    },
+    options: {
+      plugins: {
+        title: {
+            display: true,
+            text: 'inscripciones en cada evento por cada mes/ranking de eventos con más inscritos'
+        },
+        subtitle: {
+          display: true,
+          text: 'Werruning'
+      },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }}
+  });
+
+
+
+
+  
+}
 
 function graficoLineal() {
+  const ctx = document.getElementById('lineal');
 
-    google.charts.load('current', { 'packages': ['corechart'] });
-    google.charts.setOnLoadCallback(drawChart);
-    
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses'],
-            ['2004', 1000, 400],
-            ['2005', 1170, 460],
-            ['2006', 660, 1120],
-            ['2007', 1030, 540]
-        ]);
-    
-        var options = {
-            title: 'Company Performance',
-            curveType: 'function',
-            legend: { position: 'bottom' }
-        };
-    
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    
-        chart.draw(data, options);
-    }
-
-
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Enero', 'Febrero', 'marzo', 'abril', 'mayo', 'Junio', 'Julio', 'Agosto', 'septiembre', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre', 'diciembre'],
+      datasets: [{
+        label: '€',
+        data: [2900, 2040, 1900, 1500, 1200, 1800, 2500, 1300, 2000, 1700, 1200, 2400, 2350, 1800],
+        borderWidth: 2
+      }]
+    },
+    options: {
+      plugins: {
+        title: {
+            display: true,
+            text: 'Ganancias del año 2022 por inscripciones'
+        },
+        subtitle: {
+          display: true,
+          text: 'Werruning'
+      },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }}
+  });
 }
 
 
