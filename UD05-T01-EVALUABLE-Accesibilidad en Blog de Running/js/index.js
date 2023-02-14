10c4904828daac7b54002c2b47338a3e0de030b4
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-02-15 00:33:40 +0100

    mejoras

--- UD05-T01-EVALUABLE-Accesibilidad en Blog de Running/js/index.js
+++ UD05-T01-EVALUABLE-Accesibilidad en Blog de Running/js/index.js
@@ -10,7 +10,8 @@ function temaOscuro() {
   document.querySelector("html").setAttribute("data-bs-theme", "dark");
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
   document.querySelector("nav").setAttribute("class", "d-flex navbar navbar-expand-md rounded-2 row navoscuro");
-  //document.querySelector("nav").setAttribute("class", "d-flex navbar navbar-expand-md rounded-2 row navoscuro");
+  document.querySelector("footer").setAttribute("class", "position-relative p-5 footeroscuro");
+  document.getElementById("logowe").setAttribute("src", "images/imagerunningoscuro.svg");
   console.log("sale");
 }
 
@@ -19,6 +20,8 @@ function temaClaro() {
   document.querySelector("html").setAttribute("data-bs-theme", "light");
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
   document.querySelector("nav").setAttribute("class", "d-flex navbar navbar-expand-md rounded-2 row");
+  document.querySelector("footer").setAttribute("class", "position-relative p-5");
+  document.getElementById("logowe").setAttribute("src", "images/imagerunning.svg");
   console.log("sale");
 }
 
